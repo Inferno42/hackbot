1859ad699670c984afca71a9f3b40f4bbf4f29c3
Christian Little <Inferno4289@gmail.com>
2015-07-06 03:33:18 -0400

    Now lists commands when told "hackbot commands" Has its own command list and channel list as well.

--- hackbot.py
+++ hackbot.py
@@ -10,19 +10,45 @@ nick = "hackbot"
 def SendMessage(message):
     print("Sending Message")
     global websocket
-    command = ['chat', '$\\blue{\\text{' + message + '}}$']
+    #command = ['chat', '$\\blue{\\text{' + message + '}}$']
+    command = ['chat', message]
     command = json.dumps(command)
     yield from websocket.send(command)
 
 @asyncio.coroutine
 def Join():
     global websocket
-    websocket = yield from websockets.connect('ws://hack.chat:6060')
+    websocket = yield from websockets.connect('wss://hack.chat/chat-ws')
     command = ['join', channel, nick]
     command = json.dumps(command)
     print(command)
     yield from websocket.send(command)
 
+def WriteChannel(channel):
+    target = open("channel_list", 'r+')
+    with target as search:
+        for line in search:
+            line = line.rstrip()
+            if channel == line:
+                yield from SendMessage("That channel already exists though :(")
+                return 0
+        target.write("https://hack.chat/?" + channel + "\n")
+
+def ListChannels():
+    target = open("channel_list", 'r')
+    with target as search:
+        for line in search:
+            line = line.rstrip()
+            yield from SendMessage(line)
+
+def ListCommands():
+    target = open("command_list", 'r')
+    with target as search:
+        for line in search:
+            line = line.rstrip()
+            yield from SendMessage(line)
+
+@asyncio.coroutine
 def ParseRecv(response):
     parsed = json.loads(response)
     nick = parsed['nick']
@@ -41,6 +67,16 @@ def ParseRecv(response):
 
     elif nick != '*': #No matter the nick
         if text.find("hackbot") == 0:
+            if text.find("new channel") == 8:
+                channel = text.split("new channel ")
+                yield from WriteChannel(channel[1])
+
+            if text.find("list channels") == 8:
+                yield from ListChannels()
+
+            if text.find("commands") == 8:
+                yield from ListCommands()
+                
             if len(text) == 7:  
                 yield from SendMessage('Yes?')
             if nick == "Inferno":
